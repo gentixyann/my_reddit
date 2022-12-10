2db677de6a87f1f27436d6e82682acac0880acb1
Gento <a551.return@gmail.com>
2022-12-10 21:43:46 +0900

    firebaseの設定（たぶん）完了

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:my_reddit/features/auth/screens/login_screen.dart';
 import 'package:my_reddit/theme/pallete.dart';
 
-void main() {
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
