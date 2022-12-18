e80d3c2092009e3e5636269225a583066721ab40
Gento <a551.return@gmail.com>
2022-12-18 13:52:07 +0900

    google loginでfirestoreに保存

--- lib/features/auth/repository/auth_repository.dart
+++ lib/features/auth/repository/auth_repository.dart
@@ -2,7 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:my_reddit/core/constants/constants.dart';
+import 'package:my_reddit/core/constants/firebase_constants.dart';
 import 'package:my_reddit/core/providers/firebase_providers.dart';
+import 'package:my_reddit/models/user_model.dart';
 
 final authRepositoryProvider = Provider((ref) => AuthRepository(
       firestore: ref.read(firestoreProvider),
@@ -23,6 +26,9 @@ class AuthRepository {
         _firestore = firestore,
         _googleSignIn = googleSignIn;
 
+  CollectionReference get _users =>
+      _firestore.collection(FirebaseConstants.usersCollection);
+
   void signInWithGoogle() async {
     try {
       final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
@@ -36,6 +42,28 @@ class AuthRepository {
       UserCredential userCredential =
           await _auth.signInWithCredential(credential);
       print(userCredential.user?.email);
+
+      UserModel userModel;
+
+      userModel = UserModel(
+        name: userCredential.user!.displayName ?? 'No Name',
+        profilePic: userCredential.user!.photoURL ?? Constants.avatarDefault,
+        banner: Constants.bannerDefault,
+        uid: userCredential.user!.uid,
+        isAuthenticated: true,
+        karma: 0,
+        awards: [
+          'awesomeAns',
+          'gold',
+          'platinum',
+          'helpful',
+          'plusone',
+          'rocket',
+          'thankyou',
+          'til',
+        ],
+      );
+      await _users.doc(userCredential.user!.uid).set(userModel.toMap());
     } catch (e) {
       print(e);
     }
