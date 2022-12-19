5e35975107fc8ffd9cb1910c0ad9b9d9cc972b8d
Gento <a551.return@gmail.com>
2022-12-19 23:27:15 +0900

    google login にLoader widget追加

--- lib/features/auth/controlller/auth_controller.dart
+++ lib/features/auth/controlller/auth_controller.dart
@@ -2,22 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:my_reddit/core/utils.dart';
 import 'package:my_reddit/features/auth/repository/auth_repository.dart';
+import 'package:my_reddit/models/user_model.dart';
 
-final authControllerProvider = Provider(
+final userProvider = StateProvider<UserModel?>((ref) => null);
+
+final authControllerProvider = StateNotifierProvider<AuthController, bool>(
   (ref) => AuthController(
-    authRepository: ref.read(authRepositoryProvider),
+    authRepository: ref.watch(authRepositoryProvider),
+    ref: ref,
   ),
 );
 
-class AuthController {
+class AuthController extends StateNotifier<bool> {
   final AuthRepository _authRepository;
+  final Ref _ref;
   AuthController({
     required AuthRepository authRepository,
-  }) : _authRepository = authRepository;
+    required Ref ref,
+  })  : _authRepository = authRepository,
+        _ref = ref,
+        super(false); // loading
 
   void signInWithGoogle(BuildContext context) async {
+    state = true;
     final user = await _authRepository.signInWithGoogle();
+    state = false;
     // もしエラーならlでshowSnackBar、OKならrでnull
-    user.fold((l) => showSnackBar(context, l.message), (r) => null);
+    user.fold(
+        (l) => showSnackBar(context, l.message),
+        (userModel) =>
+            _ref.read(userProvider.notifier).update((state) => userModel));
   }
 }
--- lib/features/auth/screens/login_screen.dart
+++ lib/features/auth/screens/login_screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:my_reddit/core/common/loader.dart';
 import 'package:my_reddit/core/common/sign_in_button.dart';
 import 'package:my_reddit/core/constants/constants.dart';
+import 'package:my_reddit/features/auth/controlller/auth_controller.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    // StateNotifierProviderのauthControllerProviderでboolが方であるから
+    final isLoading = ref.watch(authControllerProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: Image.asset(
@@ -23,28 +29,30 @@ class LoginScreen extends StatelessWidget {
           )
         ],
       ),
-      body: Column(
-        children: [
-          const SizedBox(height: 10),
-          const Text(
-            'Dive into anything',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 0.5,
+      body: isLoading
+          ? const Loader()
+          : Column(
+              children: [
+                const SizedBox(height: 10),
+                const Text(
+                  'Dive into anything',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 0.5,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Image.asset(
+                    Constants.loginEmotePath,
+                    height: 400,
+                  ),
+                ),
+                const SignInButton()
+              ],
             ),
-          ),
-          const SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.all(8),
-            child: Image.asset(
-              Constants.loginEmotePath,
-              height: 400,
-            ),
-          ),
-          const SignInButton()
-        ],
-      ),
     );
   }
 }
